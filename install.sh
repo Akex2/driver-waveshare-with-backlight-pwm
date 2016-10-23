30275d8a2963a1458313d1d46b4dd734c1814826
Akex2 <schointss@gmail.com>
2016-10-23 11:44:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo apt-get install -y python3-pip libudev-dev
 sudo pip-3.2 install python-uinput pyudev
 #if pip-3.2 can't be found, please use pip3
 #sudo pip3 install python-uinput pyudev
-cd..
+cd ..
 
 sudo cp touch.py /usr/bin/
 sudo cp pwm.py /usr/bin/
