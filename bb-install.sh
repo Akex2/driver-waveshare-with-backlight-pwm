8e6ba817ed3d194476718b1ed325be0da496a59d
Akex2 <schointss@gmail.com>
2017-06-05 12:02:06 +0200

    Update bb-install.sh

--- bb-install.sh
+++ bb-install.sh
@@ -3,13 +3,13 @@
 chmod +x *.sh *.py
 sudo apt-get update
 sudo apt-get install python-dev python-pip
-sudo pip install wiringpi2
+#sudo pip install wiringpi2
 sudo pip-3.2 install python-uinput pyudev
 sudo apt-get install git-core
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-git pull origin
-./build
+#git clone git://git.drogon.net/wiringPi
+#cd wiringPi
+#git pull origin
+#./build
 #sudo patch -b /boot/config.txt 7inch.patch
 sudo apt-get install -y python3-pip libudev-dev
 sudo pip-3.2 install python-uinput pyudev
@@ -18,9 +18,9 @@ sudo pip-3.2 install python-uinput pyudev
 cd ..
 
 sudo cp touch.py /usr/bin/
-sudo cp pwm.py /usr/bin/
+sudo cp digitalBB.py /usr/bin/
 sudo cp touch.sh /etc/init.d/
-sudo cp pwm.sh /etc/init.d/
+sudo cp digitalBB.sh /etc/init.d/
 sudo cp toto.txt /usr/bin/
 
 sudo chmod +x /usr/bin/touch.py
